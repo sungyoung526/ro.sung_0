07a9161964671bde27f87818efe491f55cf44f11
sung_0 <sung_0@SunnyFactory-MacBookProui-MacBookPro.local>
2019-11-25 15:20:39 +0900

    Updates

---
+++ precache-manifest.56f1f15fbbceb297f269eaa540b1f1c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9c0bebac498597cfbe97c3ee3166a4",
+    "url": "/sungyoung/index.html"
+  },
+  {
+    "revision": "526985a139ed0ae68e78",
+    "url": "/sungyoung/static/js/2.e3f3a5af.chunk.js"
+  },
+  {
+    "revision": "22f406203a5662a1dc93",
+    "url": "/sungyoung/static/js/main.030000a1.chunk.js"
+  },
+  {
+    "revision": "85aeae282aad8c78bbf7",
+    "url": "/sungyoung/static/js/runtime-main.3cede735.js"
+  }
+]);
